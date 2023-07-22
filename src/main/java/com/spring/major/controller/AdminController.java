0d7f8347c6d2ef6d2797f2358ba98c659160de60
Night <dranhata@gmail.com>
2023-07-22 23:28:50 +0700

    #2 Create & Read Category

--- src/main/java/com/spring/major/controller/AdminController.java
+++ src/main/java/com/spring/major/controller/AdminController.java
@@ -1,10 +1,19 @@
 package com.spring.major.controller;
 
+import com.spring.major.model.Category;
+import com.spring.major.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
+
+    @Autowired
+    CategoryService categoryService;
 
     @GetMapping("/admin")
     public String adminHome(){
@@ -12,12 +21,20 @@ public class AdminController {
     }
 
     @GetMapping("/admin/categories")
-    public String getCat(){
+    public String getCat(Model model){
+        model.addAttribute("categories", categoryService.getAllCategory());
         return "categories";
     }
 
     @GetMapping("/admin/categories/add")
-    public String getCatAdd(){
+    public String getCatAdd(Model model){
+        model.addAttribute("category", new Category());
         return "categoriesAdd";
+    }
+
+    @PostMapping("/admin/categories/add")
+    public String postCatAdd(@ModelAttribute("category") Category category){
+        categoryService.addCategory(category);
+        return "redirect:/admin/categories";
     }
 }
---
+++ src/main/java/com/spring/major/service/CategoryService.java
@@ -0,0 +1,23 @@
+package com.spring.major.service;
+
+import com.spring.major.model.Category;
+import com.spring.major.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategory(){
+        return categoryRepository.findAll();
+    }
+
+    public void addCategory(Category category){
+        categoryRepository.save(category);
+    }
+}
