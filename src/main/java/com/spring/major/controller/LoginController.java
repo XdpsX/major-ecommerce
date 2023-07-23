ae4ca5f56cae1cdf3f5b36df4f1b2942ed2bab6f
Night <dranhata@gmail.com>
2023-07-23 15:39:45 +0700

    #13 Register

--- src/main/java/com/spring/major/controller/LoginController.java
+++ src/main/java/com/spring/major/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.spring.major.controller;
 
+import com.spring.major.model.Role;
+import com.spring.major.model.User;
 import com.spring.major.repository.RoleRepository;
 import com.spring.major.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class LoginController {
@@ -26,5 +35,17 @@ public class LoginController {
     public String registerGet(Model model){
 
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@ModelAttribute("user")User user, HttpServletRequest request) throws ServletException {
+        String password = user.getPassword();
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        List<Role> roles = new ArrayList<>();
+        roles.add(roleRepository.findById(2).get());
+        user.setRoles(roles);
+        userRepository.save(user);
+        request.login(user.getEmail(), user.getPassword());
+        return "redirect:/";
     }
 }
